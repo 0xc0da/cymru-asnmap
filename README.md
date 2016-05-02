509e85348da7df6e977bf041807b9448cb8ff266
Daniel Marques <daniel@codalabs.net>
2016-05-01 23:45:34 -0500

    Initial commit.

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
-# cymru-asnmap
-Bulk IP to ASN submission to the Team Cymru Whois server. More info: http://www.team-cymru.org/IP-ASN-mapping.html
+# cymru-asnmap.py
+
+cymru-asnmap.py is a python script to perform bulk IP submissions to the Team Cymru
+whois service.
+
+More on the Team Cymru Whois service: http://www.team-cymru.org/IP-ASN-mapping.html
+
+## Dependencies
+The netaddr module: https://pypi.python.org/pypi/netaddr
+
+```
+pip install netaddr
+```
+
+## Usage
+
+```
+usage: cymru-asnmap.py [-h] [-f] [-o FILENAME] target
+
+positional arguments:
+  target                Target to be queried.
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -f, --file            Load IPs from a file.
+  -o FILENAME, --output FILENAME
+                        Output CSV file.
+```
+
+### Examples
+
+#### CIDR
+```
+$ python cymru-asnmap.py 68.22.187.0/29
+Bulk mode; whois.cymru.com [2016-05-02 04:40:56 +0000]
+23028   | 68.22.187.0      | 68.22.187.0/24      | US | arin     | 2002-03-15 | TEAM-CYMRU - Team Cymru Inc., US
+23028   | 68.22.187.1      | 68.22.187.0/24      | US | arin     | 2002-03-15 | TEAM-CYMRU - Team Cymru Inc., US
+23028   | 68.22.187.2      | 68.22.187.0/24      | US | arin     | 2002-03-15 | TEAM-CYMRU - Team Cymru Inc., US
+23028   | 68.22.187.3      | 68.22.187.0/24      | US | arin     | 2002-03-15 | TEAM-CYMRU - Team Cymru Inc., US
+23028   | 68.22.187.4      | 68.22.187.0/24      | US | arin     | 2002-03-15 | TEAM-CYMRU - Team Cymru Inc., US
+23028   | 68.22.187.5      | 68.22.187.0/24      | US | arin     | 2002-03-15 | TEAM-CYMRU - Team Cymru Inc., US
+23028   | 68.22.187.6      | 68.22.187.0/24      | US | arin     | 2002-03-15 | TEAM-CYMRU - Team Cymru Inc., US
+23028   | 68.22.187.7      | 68.22.187.0/24      | US | arin     | 2002-03-15 | TEAM-CYMRU - Team Cymru Inc., US
+```
+
+
+#### List of IPs from a file
+```
+$ python cymru-asnmap.py -f input.txt
+Bulk mode; whois.cymru.com [2016-05-02 04:34:05 +0000]
+23028   | 68.22.187.5      | 68.22.187.0/24      | US | arin     | 2002-03-15 | TEAM-CYMRU - Team Cymru Inc., US
+6079    | 207.229.165.18   | 207.229.128.0/18    | US | arin     |            | RCN-AS - RCN, US
+701     | 198.6.1.65       | 198.6.0.0/16        | US | arin     |            | UUNET - MCI Communications Services, Inc. d/b/a Verizon Business, US
+```
+
+# License
+* Copyright (c) 2016, Daniel C. Marques - Check the LICENSE file for license.
+* Team Cymru WHOIS Copyright (c) 2015 Team Cymru. All Rights Reserved.
---
+++ cymru-asnmap.py
@@ -0,0 +1,115 @@
+#!/usr/bin/python
+"""Copyright (c) 2016, Daniel C. Marques
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice, this
+  list of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+
+* Neither the name of cymru-asnmap nor the names of its
+  contributors may be used to endorse or promote products derived from
+  this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+cymru-asnmap.py is a wrapper to perform bulk IP submissions to the Team Cymru
+whois service.
+
+Team Cymru Whois service: http://www.team-cymru.org/IP-ASN-mapping.html
+"""
+
+import socket
+import csv
+import argparse
+from netaddr import IPNetwork
+
+
+def to_csv(data, filename):
+    """ Parses and cleans up the query response and generates a CSV file with
+    the result.
+    """
+    with open(filename, "wb") as csvfile:
+        csvwriter = csv.writer(csvfile, delimiter=';',
+                               quotechar='"', quoting=csv.QUOTE_MINIMAL)
+
+        csvwriter.writerow(["AS", "IP", "BGP Prefix", "CC", "Registry",
+                            "Allocated", "Info", "AS Name"])
+
+        for line in data.split('\n')[1:]:
+            csvwriter.writerow([column.rstrip(" ").lstrip(" ")
+                                for column in line.split("|")])
+
+
+def query(bulk_query):
+    """ Connects to the whois server and sends the bulk query. Returns the
+    result of this query.
+    """
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect(("whois.cymru.com", 43))
+        s.sendall(bulk_query)
+        reply = s.recv(1024)
+        data = reply
+        # Gets data until an empty line is found.
+        while reply:
+            reply = s.recv(1024)
+            data += reply
+    except Exception as e:
+        raise e
+    finally:
+        s.close()
+
+    return data
+
+
+def main():
+    try:
+        parser = argparse.ArgumentParser()
+        parser.add_argument("target", help="Target to be queried (CIDR or filename).")
+        parser.add_argument("-f", "--file", help="Loads the IPs from a file.",
+                            dest="from_file", action="store_true")
+        parser.add_argument("-o", "--output", help="Output CSV file.",
+                            dest="filename")
+        args = parser.parse_args()
+
+        if args.filename:
+            filename = argparse.filename
+        else:
+            filename = "output-asnmap.csv"
+
+        if args.from_file:
+            with open(args.target, "rb") as input_file:
+                ips = input_file.read().rstrip("\n")
+        else:
+            net = IPNetwork(args.target)
+            ips = "\n".join([str(ip) for ip in list(net)])
+
+        # Creates the file for bulk submission
+        bulk_query = "begin\nverbose\n%s\nend" % ips
+
+        response = query(bulk_query)
+
+        print response
+
+        to_csv(response, filename)
+
+    except Exception as e:
+        print "[!] Unable to proceed. Error: %s" % e
+
+if __name__ == '__main__':
+    main()
